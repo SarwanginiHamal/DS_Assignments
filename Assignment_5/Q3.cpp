1075da0eecb357c4346a8fd3f144180c87c6c307
Sarwangini <hamalsarwangini@gmail.com>
2025-10-26 14:57:57 +0530

    Create Q3.cpp

---
+++ Assignment_5/Q3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+    Node* head;
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = newNode;
+    }
+
+    void printMiddle() {
+        if (!head) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        Node* slow = head;
+        Node* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        cout << "Middle element: " << slow->data << endl;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data;
+            if (temp->next) cout << "->";
+            temp = temp->next;
+        }
+        cout << "->NULL" << endl;
+    }
+
+    Node* getHead() { return head; }
+};
+int main() {
+    LinkedList list;
+    int arr[] = {1,2,3,4,5};
+    for (int v : arr) list.insertAtEnd(v);
+    list.display();     
+    list.printMiddle();   
+    return 0;
+}
