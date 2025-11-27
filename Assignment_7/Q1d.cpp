9a6bcb893306da1c2da58861858be1a9e7df4cfe
Sarwangini <hamalsarwangini@gmail.com>
2025-11-27 09:51:25 +0530

    Create Q1d.cpp

---
+++ Assignment_7/Q1d.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class MergeSort {
+private:
+    int arr[100];
+    int n;
+    void merge(int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int L[100], R[100];
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int i = 0; i < n2; i++)
+            R[i] = arr[mid + 1 + i];
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    void mergeSort(int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+            merge(left, mid, right);
+        }
+    }
+
+public:
+    void input() {
+        cout << "Enter number of elements: ";
+        cin >> n;
+        cout << "Enter " << n << " elements:\n";
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+    }
+    void sortArray() {
+        mergeSort(0, n - 1);
+    }
+    void display() {
+        cout << "\nSorted Array: ";
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    MergeSort m;
+    m.input();
+    m.sortArray();
+    m.display();
+    return 0;
+}
