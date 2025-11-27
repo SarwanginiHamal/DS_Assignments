c36869f2f60a48fc4898a7cf90040566fd503991
Sarwangini <hamalsarwangini@gmail.com>
2025-11-27 12:34:04 +0530

    Create Assignment_8_4.cpp

---
+++ Additional_Questions/Assignment_8_4.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* buildTree(const vector<int>& a) {
+    if (a.empty() || a[0] == -1) return nullptr;
+    int n = a.size();
+    TreeNode* root = new TreeNode(a[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    int i = 1;
+    while (!q.empty() && i < n) {
+        TreeNode* cur = q.front();
+        q.pop();
+        if (i < n && a[i] != -1) {
+            cur->left = new TreeNode(a[i]);
+            q.push(cur->left);
+        }
+        i++;
+        if (i < n && a[i] != -1) {
+            cur->right = new TreeNode(a[i]);
+            q.push(cur->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+vector<int> rightView(TreeNode* root) {
+    vector<int> res;
+    if (!root) return res;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        int sz = q.size();
+        for (int i = 0; i < sz; ++i) {
+            TreeNode* cur = q.front();
+            q.pop();
+            if (i == sz - 1) res.push_back(cur->val);
+            if (cur->left) q.push(cur->left);
+            if (cur->right) q.push(cur->right);
+        }
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        TreeNode* root = buildTree(a);
+        vector<int> ans = rightView(root);
+        for (int i = 0; i < (int)ans.size(); ++i) {
+            if (i) cout << " ";
+            cout << ans[i];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
